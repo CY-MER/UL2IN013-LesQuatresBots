8f3a7ff2dbd8fc15c3b44a7ff4a215b78a3dd638
yasmineboukercha75-droid <yasmineboukercha75@gmail.com>
2026-02-04 01:52:57 +0100

    Update Main_test.py

--- Main_test.py
+++ Main_test.py
@@ -11,7 +11,7 @@ def test_initialisation():
     assert y == 0.0, "y initial incorrect"
     assert angle == 0, "rotation initiale incorrecte"
     assert vitesse == 0.0, "vitesse initiale incorrecte"
-    print("✔ Test initialisation OK")
+    printf(" Test initialisation OK")
 
 def test_avancer():
     robot = Robot(0, 0, 0)
@@ -22,7 +22,7 @@ def test_avancer():
     assert x == 10.0, "avancer() : x incorrect"
     assert y == 0.0, "avancer() : y incorrect"
     assert vitesse == 5.0, "avancer() : vitesse incorrecte"
-    print("✔ Test avancer OK") 
+    printf("Test avancer OK") 
 
 def test_tourner():
     robot = Robot()
@@ -31,7 +31,7 @@ def test_tourner():
 
     robot.tourner(300)
     assert robot.rotation == 30, "tourner() modulo 360 incorrect"
-    print("✔ Test tourner OK")
+    printf(" Test tourner OK")
 
 def test_capteur():
     robot = Robot(0, 0, 0, sens=10)
@@ -45,7 +45,7 @@ def test_capteur():
 
     assert cx == 0.0, "capteur x après rotation incorrect"
     assert cy == 10.0, "capteur y après rotation incorrect"
-    print("✔ Test capteur OK")
+    printf(" Test capteur OK")
 
 
 def test_carre():
@@ -58,7 +58,7 @@ def test_carre():
     assert math.isclose(x, 0.0, abs_tol=0.01), "carré : x final incorrect"
     assert math.isclose(y, 0.0, abs_tol=0.01), "carré : y final incorrect"
     assert angle == 0, "carré : rotation finale incorrecte"
-    print("✔ Test carré OK")
+    printf(" Test carré OK")
   
 def main():
     print("=== DÉBUT DES TESTS ===")
@@ -67,7 +67,7 @@ def main():
     test_tourner()
     test_capteur()
     test_carre()
-    print("=== TOUS LES TESTS SONT VALIDES ✅ ===")
+    printf("=== TOUS LES TESTS SONT VALIDES ✅ ===")
 
 
 main()
