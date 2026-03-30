edfffafd24c5405cc94967f994c84767456550a6
yasmineboukercha75-droid <yasmineboukercha75@gmail.com>
2026-03-30 22:08:43 +0200

    update init

--- UL2IN013-LesQuatresBots/models/__init__.py
+++ UL2IN013-LesQuatresBots/models/__init__.py
@@ -2,5 +2,6 @@ from .robot import Robot
 from .robot_vecteur import RobotVecteur
 from .point import Point
 from .vecteur2D import Vecteur2D
+from .obstacle import Obstacle 
 
-__all__ = ['Robot', 'RobotVecteur', 'Point', 'Vecteur2D']
+__all__ = ['Robot', 'RobotVecteur', 'Point', 'Vecteur2D','Obstacle']
