f5e1277dc8585e1f33a20c2e50e7702338b2803f
yasmineboukercha75-droid <yasmineboukercha75@gmail.com>
2026-04-06 20:40:19 +0200

    update strategie avancer/tourner/stop

--- UL2IN013-LesQuatresBots/controllers/strategie.py
+++ UL2IN013-LesQuatresBots/controllers/strategie.py
@@ -1,3 +1,5 @@
+import math
+
 class Strategie:
     """Classe de base pour les stratégies"""
 
@@ -11,65 +13,49 @@ class Avancer(Strategie):
     def __init__(self, distance, pas=5):
         self.distance = distance
         self.pas = pas
-        self.distance_parcourue = 0
-        self.terminee = False
+        self.positions_depart = None 
 
-    def update(self, robot, obstacles=None):
-        if self.terminee:
-            return
+    def update(self, robot, dt):
+        if self.positions_depart is None:
+            self.positions_depart = (robot.position.x , robot.position.y)
 
-        if self.distance_parcourue < self.distance:
-            deplacement = min(self.pas, self.distance - self.distance_parcourue)
-            # sauvegarder position avant déplacement
-            old_x, old_y, _, _ = robot.get_location()
-            robot.avancer(deplacement)
-            # vérifier collision si obstacles fournis
-            if obstacles:
-                for obstacle in obstacles:
-                    if obstacle.collision(robot):
-                        # revenir en arrière
-                        robot.x = old_x
-                        robot.y = old_y
-                        robot.vitesse = 0
-                        robot.tourner(10)  # tourne légèrement pour éviter
-                        print("Collision ! Le robot tourne.")
-                        return
- 
-            self.distance_parcourue += deplacement
-        else:
-            self.terminee = True
+        x0 ,y0 = self.positions_depart
+        x , y = robot.position.x , robot.posotion.y
 
+        distance_parcourue = math.sqrt((x-x0)**2 + (y-y0)**2)
+
+        if distance_parcourue >= self.distance:
+            robot.stop()
+            return 
+        
+        robot.avancer(self.vitesse)
+        
 
 class Tourner(Strategie):
     """Fait tourner le robot d'un angle donné, petit à petit"""
 
-    def __init__(self, angle, pas_angle=5):
+    def __init__(self, angle, vitesse):
         self.angle = angle
-        self.pas_angle = pas_angle
-        self.angle_tourne = 0
-        self.terminee = False
+        self.vitesse = vitesse
+        self.rotation_depart = None 
 
     def update(self, robot):
-        if self.terminee:
-            return
+        if self.position_depart is None :
+            self.rotation_depart = (robot.rotation)
 
-        if self.angle_tourne < self.angle:
-            rotation = min(self.pas_angle, self.angle - self.angle_tourne)
-            robot.tourner(rotation)
-            self.angle_tourne += rotation
-        else:
-            self.terminee = True
+        rotation = abs(robot.rotation - self.rotation_depart)
 
+        if rotation >= self.angle:
+            robot.stop()
+            return 
+        
+        robot.tourner(self.vitesse)
 
 class Stop(Strategie):
     """Arrête le robot"""
 
-    def __init__(self):
-        self.terminee = False
-
     def update(self, robot):
-        robot.vitesse = 0
-        self.terminee = True
+        robot.stop()
 
 
 class Carre(Strategie):
